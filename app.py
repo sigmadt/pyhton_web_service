89b7c43d549ec4a13665f11d783a5a0186eed96b
sayfulindmitry <54313653+sayfulindmitry@users.noreply.github.com>
2019-08-23 17:43:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,13 +22,13 @@ def action():
 def post():
     data = request.get_json()
 
-    return jsonify(str(content) + ' ' + str(uuid.uuid1()))
+    return jsonify(str(data) + ' ' + str(uuid.uuid1()))
 
 
 
-#Examples:
+#Example:
 
-#1. С помощью GET запроса посчитаем квадрат числа, переданного в строку
+#С помощью GET запроса посчитаем квадрат числа, переданного в строку
 
 @app.route('/square/<num>')
 def square(num):
@@ -39,11 +39,3 @@ def square(num):
     response = {'text': squared_num, 'current_time': current_time, 'status code': 200}
     
     return jsonify(response)
-
-#2. С помощью POST получим переданные данные в Json + uuid
-
-@app.route('/post', methods=['POST'])
-def post():
-    data = request.get_json()
-
-    return jsonify(str(content) + ' ' + str(uuid.uuid1()))
